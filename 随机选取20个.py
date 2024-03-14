82f823ec5be8ebec17281effb1788978ac672958
Caiziying <11@qq.com>
2024-03-14 19:49:44 +0800

    main

--- 随机选取20个.py
+++ 随机选取20个.py
@@ -1,17 +1,22 @@
 import os
 import random
+import shutil
 
 # 定义图片所在文件夹路径
 image_folder = 'D:\\A8\\Reconnaissance\\P1地雷'
+target_folder = 'D:\\A8\\Reconnaissance\\新建文件夹'
 
 # 获取文件夹中所有图片的文件名列表
 image_files = os.listdir(image_folder)
 
 # 从文件名列表中随机选择20个文件名
-random_files = random.sample(image_files, 2)
+random_files = random.sample(image_files, 5)
+
+# 遍历随机选择的文件名列表，复制文件到目标文件夹
+for file_name in random_files:
+    source_path = os.path.join(image_folder, file_name)
+    target_path = os.path.join(target_folder, file_name)
+    shutil.copyfile(source_path, target_path)
 
 # 打印所选文件名，以便检查
 print("随机选择的文件名列表：", random_files)
-
-# 现在你可以修改你的代码，只对这20张图片进行检测
-# 例如，你可以使用这些文件名来读取图片并进行检测
